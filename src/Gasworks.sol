396257a3c900e2f076899445efbcbea62dd23186
Gaonuk <rodrigo.gaona@uc.cl>
2023-04-17 15:47:04 -0400

    :rotating_light: Fix lint

--- src/Gasworks.sol
+++ src/Gasworks.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.17.0;
 
 import "gsn/ERC2771Recipient.sol";
 import "solmate/tokens/ERC20.sol";
--- src/interfaces/IExchangeIssuanceZeroEx.sol
+++ src/interfaces/IExchangeIssuanceZeroEx.sol
@@ -1,6 +1,6 @@
 //    SPDX-License-Identifier: Unlicensed
 
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.17.0;
 
 pragma experimental ABIEncoderV2;
 
