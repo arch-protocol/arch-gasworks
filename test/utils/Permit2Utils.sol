4c82560cb481831102d9f00fda4958bd5cca87ed
Gaonuk <rodrigo.gaona@uc.cl>
2023-04-24 17:43:43 -0400

    :sparkles: Add Permit2Utils for Permit2 testcases

---
+++ test/utils/Permit2Utils.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17.0;
+
+import {Test} from "forge-std/Test.sol";
+import {ISignatureTransfer} from "permit2/src/interfaces/ISignatureTransfer.sol";
+
+contract Permit2Utils is Test {
+    function getTransferDetails(address to, uint256 amount)
+        internal
+        pure
+        returns (ISignatureTransfer.SignatureTransferDetails memory)
+    {
+        return ISignatureTransfer.SignatureTransferDetails({to: to, requestedAmount: amount});
+    }
+
+    function getSignature(
+        ISignatureTransfer.PermitTransferFrom memory permit,
+        uint256 privateKey,
+        bytes32 typehash,
+        bytes32 witness,
+        bytes32 domainSeparator,
+        bytes32 tokenPermissionsHash,
+        address caller
+    ) internal returns (bytes memory sig) {
+        bytes32 tokenPermissions = keccak256(abi.encode(tokenPermissionsHash, permit.permitted));
+
+        bytes32 msgHash = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                domainSeparator,
+                keccak256(abi.encode(typehash, tokenPermissions, caller, permit.nonce, permit.deadline, witness))
+            )
+        );
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, msgHash);
+        return bytes.concat(r, s, bytes1(v));
+    }
+}
