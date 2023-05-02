984d8f385b8e9f79ae2609c035ed847f44b2d2c7
Gaonuk <rodrigogaonagonzalez@gmail.com>
2023-05-02 16:34:29 -0400

    :hammer: Modify arch request to accept different tokens for trade

--- scripts/fetch-arch-quote.js
+++ scripts/fetch-arch-quote.js
@@ -15,44 +15,45 @@ async function generateJwt(payload) {
   return jwt;
 }
 
-async function main(quantity, token, operation) {
+async function main(quantity, basketAddress, tokenAddress, operation) {
   const qty = encoder.decode(["uint256"], quantity)[0]
-  const tokenAddress = encoder.decode(["address"], token)[0]
+  const basket = encoder.decode(["address"], basketAddress)[0]
+  const token = encoder.decode(["address"], tokenAddress)[0]
   const opt = encoder.decode(["bool"], operation)[0]
 
-  archTokens[tokenAddress].basketAmountInWei = qty.toString()
-
-  const jwt = await generateJwt(archTokens[tokenAddress])
+  archTokens[basket].basketAmountInWei = qty.toString()
+  archTokens[basket].tokenAddress = token
+  const jwt = await generateJwt(archTokens[basket])
   const baseUrl = "https://dev-api.archfinance.io/basket-issuance/"
   const opType = opt ? "issuance" : "redemption"
-  const quoteUrl = `${baseUrl}${opType}-components/${tokenAddress}?${qs.stringify(
-    archTokens[tokenAddress]
+  const quoteUrl = `${baseUrl}${opType}-components/${basket}?${qs.stringify(
+    archTokens[basket]
   )}`
   try {
     const response = await axios.get(quoteUrl, {
       headers: { Authorization: `Bearer ${jwt}` },
     })
-  
+
     const quote = response.data
     const value = opt ? quote.maxAmountInWei : quote.minAmountInWei
     const data = quote.callInstructions ? quote.callInstructions : quote.encodedComponentQuotes
     const encodedType = quote.callInstructions ? "tuple(address target, address allowanceTarget, address sellToken, uint256 sellAmount, address buyToken, uint256 minBuyAmount, bytes callData)[]" : "bytes[]"
     encoded = encoder.encode([encodedType, "uint256"], [data, value])
-  
+
     process.stdout.write(encoded)
   } catch (error) {
     console.log(error)
   }
-  
+
 }
 
 const args = process.argv.slice(2)
 
-if (args.length != 3) {
+if (args.length != 4) {
   console.log(`please supply the correct parameters:
-    quantity token operation
+    quantity basket token operation
   `)
   process.exit(1)
 }
 
-main(args[0], args[1], args[2])
+main(args[0], args[1], args[2], args[3])
