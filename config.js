578802db6fdce53d9c2ad382627e52af61b91e3e
Gaonuk <rodrigogaonagonzalez@gmail.com>
2023-05-09 19:26:44 -0400

    :sparkles: Add AAGG config for quote fetching

--- config.js
+++ config.js
@@ -18,11 +18,21 @@ const ADDYTokenParams = {
   basketAddress: '0xE15A66b7B8e385CAa6F69FD0d55984B96D7263CF',
 }
 
+const AAGGTokenParams = {
+  tokenAddress: '0x6b175474e89094c44da98b954eedeac495271d0f',
+  slippagePercentageProportion: 0.05,
+  networkId: 137,
+  issuanceModuleAddress: '0x60F56236CD3C1Ac146BD94F2006a1335BaA4c449',
+  isDebtIssuance: true,
+  basketAmountInWei: '',
+  basketAddress: '0xAfb6E8331355faE99C8E8953bB4c6Dc5d11E9F3c',
+}
 
 
 const archTokens = {
   "0x6cA9C8914a14D63a6700556127D09e7721ff7D3b": AP60TokenParams,
   "0xE15A66b7B8e385CAa6F69FD0d55984B96D7263CF": ADDYTokenParams,
+  "0xAfb6E8331355faE99C8E8953bB4c6Dc5d11E9F3c": AAGGTokenParams,
 }
 
 module.exports = {
