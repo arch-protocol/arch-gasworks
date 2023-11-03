f58e2905277a9797e23447fa0057b6e4aef4136d
mvargas33 <mvargas33@protonmail.com>
2023-11-03 17:36:45 -0300

    ✨ Modify redeem test to use JSON

--- test/permitTwo/redeem.t.sol
+++ test/permitTwo/redeem.t.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17.0;
 
+import "forge-std/StdJson.sol";
 import { Test } from "forge-std/Test.sol";
 import { console } from "forge-std/console.sol";
 import { Gasworks } from "src/Gasworks.sol";
@@ -17,60 +18,69 @@ contract GaslessTest is Test, Permit2Utils {
                               VARIABLES
     //////////////////////////////////////////////////////////////*/
     using SafeERC20 for IERC20;
+    using stdJson for string;
+
+    string root;
+    string path;
+    string json;
 
     /*//////////////////////////////////////////////////////////////
                               SET UP
     //////////////////////////////////////////////////////////////*/
     function setUp() public {
         addLabbels();
+        root = vm.projectRoot();
     }
 
     /*//////////////////////////////////////////////////////////////
                               SUCCESS
     //////////////////////////////////////////////////////////////*/
 
-    function redeemChamber(uint256 chainId, address archToken, address toToken) public {
+    function redeemChamber(
+        uint256 chainId,
+        uint256 blockNumber,
+        address archToken,
+        uint256 archTokenAmount,
+        address toToken,
+        uint256 minReceiveAmount,
+        ITradeIssuerV2.ContractCallInstruction[] memory contractCallInstructions
+    ) public {
         Gasworks gasworks;
         address issuerWizard;
         address uniswapPermit2;
         if (chainId == POLYGON_CHAIN_ID) {
-            vm.createSelectFork("polygon");
+            vm.createSelectFork("polygon", blockNumber);
             gasworks = deployGasworks(chainId);
             issuerWizard = POLYGON_ISSUER_WIZARD;
             uniswapPermit2 = POLYGON_UNISWAP_PERMIT2;
         }
         if (chainId == ETH_CHAIN_ID) {
-            vm.createSelectFork("ethereum");
+            vm.createSelectFork("ethereum", blockNumber);
             gasworks = deployGasworks(chainId);
             issuerWizard = ETH_ISSUER_WIZARD;
             uniswapPermit2 = ETH_UNISWAP_PERMIT2;
         }
 
         vm.prank(ALICE);
+        console.log("1");
         IERC20(archToken).approve(uniswapPermit2, type(uint256).max);
-        uint256 amountToRedeem = 5e18;
+        console.log("2");
         uint256 previousToTokenBalance = IERC20(toToken).balanceOf(ALICE);
-
-        (
-            ITradeIssuerV2.ContractCallInstruction[] memory _contractCallInstructions,
-            uint256 minReceiveAmount
-        ) = fetchRedeemQuote(archToken, amountToRedeem, toToken);
-
         uint256 adjustedMinReceiveAmount = minReceiveAmount;
 
         if (chainId == ETH_CHAIN_ID) {
             adjustedMinReceiveAmount = (minReceiveAmount * 0) / 1000; // Avoid underbought error
         }
 
-        deal(archToken, ALICE, amountToRedeem);
+        deal(archToken, ALICE, archTokenAmount);
         uint256 previousArchTokenBalance = IERC20(archToken).balanceOf(ALICE);
 
         IGasworks.SwapCallInstruction[] memory swapCallInstructions =
-            getSwapCallsFromContractCalls(_contractCallInstructions);
+            getSwapCallsFromContractCalls(contractCallInstructions);
 
         IGasworks.RedeemData memory myRedeemData = IGasworks.RedeemData(
             archToken,
-            amountToRedeem,
+            archTokenAmount,
             toToken,
             adjustedMinReceiveAmount,
             issuerWizard,
@@ -81,7 +91,7 @@ contract GaslessTest is Test, Permit2Utils {
         uint256 currentDeadline = getFiveMinutesDeadlineFromNow();
 
         ISignatureTransfer.PermitTransferFrom memory permit = ISignatureTransfer.PermitTransferFrom({
-            permitted: ISignatureTransfer.TokenPermissions({ token: archToken, amount: amountToRedeem }),
+            permitted: ISignatureTransfer.TokenPermissions({ token: archToken, amount: archTokenAmount }),
             nonce: currentNonce,
             deadline: currentDeadline
         });
@@ -97,41 +107,93 @@ contract GaslessTest is Test, Permit2Utils {
         console.log((100 * finalToTokenBalanceDiff) / minReceiveAmount);
 
         assertGe(finalToTokenBalanceDiff, adjustedMinReceiveAmount);
-        assertEq(previousArchTokenBalance - IERC20(archToken).balanceOf(ALICE), amountToRedeem);
+        assertEq(previousArchTokenBalance - IERC20(archToken).balanceOf(ALICE), archTokenAmount);
+    }
+
+    /**
+     * Loads params and call instructions (quote) from a local json file, and then
+     * runs it to redeem a chamber
+     */
+    function runLocalRedeemQuoteTest(string memory fileName) public {
+        path = string.concat(root, fileName);
+        json = vm.readFile(path);
+        (
+            uint256 networkId,
+            uint256 blockNumber,
+            address archToken,
+            uint256 archTokenAmount,
+            address toToken,
+            uint256 minReceiveAmount,
+            ITradeIssuerV2.ContractCallInstruction[] memory contractCallInstructions
+        ) = parseRedeemQuoteFromJson(json);
+        redeemChamber(
+            networkId,
+            blockNumber,
+            archToken,
+            archTokenAmount,
+            toToken,
+            minReceiveAmount,
+            contractCallInstructions
+        );
+    }
+
+    /**
+     * Used to create json files, fetches a quote from arch and prints a JSON-readable
+     * quote in console, ready to be saved for new tests. The fork is needed to get the
+     * block number alongside the quote.
+     */
+    function testPrintQuoteToCreateATest() public {
+        vm.createSelectFork("ethereum");
+        fetchRedeemQuote(POLYGON_CHAIN_ID, POLYGON_ABAL, 10e18, POLYGON_USDC);
     }
 
     /**
      * [SUCCESS] Should redeem AAGG for AEDY using permit2
      */
     function testRedeemWithPermit2FromAaggToAedyOnPolygon() public {
-        redeemChamber(POLYGON_CHAIN_ID, POLYGON_AAGG, POLYGON_AEDY);
+        // redeemChamber(POLYGON_CHAIN_ID, POLYGON_AAGG, POLYGON_AEDY);
+        runLocalRedeemQuoteTest(
+            "/data/permitTwo/redeem/testRedeemWithPermit2FromAaggToAedyOnPolygon.json"
+        );
     }
 
     /**
      * [SUCCESS] Should redeem AMOD for USDT using permit2 [Test when supply is available]
      */
     // function testRedeemWithPermit2FromAmodToUsdtOnPolygon() public {
-    //     redeemChamber(POLYGON_CHAIN_ID, POLYGON_AMOD, POLYGON_USDT);
+    //     // redeemChamber(POLYGON_CHAIN_ID, POLYGON_AMOD, POLYGON_USDT);
+    //     runLocalRedeemQuoteTest(
+    //         "/data/permitTwo/redeem/testRedeemWithPermit2FromAmodToUsdtOnPolygon.json"
+    //     );
     // }
 
     /**
      * [SUCCESS] Should redeem ABAL for USDC using permit2
      */
     function testRedeemWithPermit2FromAbalToUsdcOnPolygon() public {
-        redeemChamber(POLYGON_CHAIN_ID, POLYGON_ABAL, POLYGON_USDC);
+        // redeemChamber(POLYGON_CHAIN_ID, POLYGON_ABAL, POLYGON_USDC);
+        runLocalRedeemQuoteTest(
+            "/data/permitTwo/redeem/testRedeemWithPermit2FromAbalToUsdcOnPolygon.json"
+        );
     }
 
     /**
      * [SUCCESS] Should redeem AEDY for CHAIN using permit2 [Most of the time fails]
      */
-    // function testRedeemWithPermit2FromAedyToChainOnEthereum() public {
-    //     redeemChamber(ETH_CHAIN_ID, ETH_AEDY, ETH_CHAIN);
-    // }
+    function testRedeemWithPermit2FromAedyToChainOnEthereum() public {
+        // redeemChamber(ETH_CHAIN_ID, ETH_AEDY, ETH_CHAIN);
+        runLocalRedeemQuoteTest(
+            "/data/permitTwo/redeem/testRedeemWithPermit2FromAedyToChainOnEthereum.json"
+        );
+    }
 
     /**
      * [SUCCESS] Should redeem ADDY for WETH using permit2 [Most of the time fails]
      */
-    // function testRedeemWithPermit2FromAddyToWethOnEthereum() public {
-    //     redeemChamber(ETH_CHAIN_ID, ETH_ADDY, ETH_WETH);
-    // }
+    function testRedeemWithPermit2FromAddyToWethOnEthereum() public {
+        // redeemChamber(ETH_CHAIN_ID, ETH_ADDY, ETH_WETH);
+        runLocalRedeemQuoteTest(
+            "/data/permitTwo/redeem/testRedeemWithPermit2FromAddyToWethOnEthereum.json"
+        );
+    }
 }
